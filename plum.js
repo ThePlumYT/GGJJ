c7ffbe2f1e587a4aeb821ed0cd34ae3cba38eeb5
ThePlumYT <60061341+ThePlumYT@users.noreply.github.com>
2020-01-20 19:19:33 +0700

    Update plum.js

--- plum.js
+++ plum.js
@@ -4,6 +4,8 @@ const config = require("./config.json");
 
 plum.on('ready', () => {
   console.log(`Logged !`);
+  if (config.activity.playing == true) {
+    bot.user.setActivity('พิมพ์ /plum');
 });
 
 plum.on('message', msg => {
